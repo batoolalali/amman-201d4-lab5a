4eba0f31a1db12cdf43ffa51e9869f4478905be1
batoolalali <abdullahbatool93@gmail.com>
2020-02-20 14:27:07 +0200

    added ArrSum function

--- app.js
+++ app.js
@@ -87,13 +87,23 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
+
 function sumArray(sumArr) { //eslint-disable-line
+    var arrSum=0;
 
+    for(var i=0; i<testArray.length; i++)
+    {
+        arrSum=sum(arrSum,testArray[i])[0];
+
+    }
+    //arrSum=arrSum/2+4.5;
+    console.log(arrSum, " Arrsum");
+    var message=`${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${arrSum} is their sum.`;
+    return [arrSum,message];
 }
-
+// sumArray();
 // Here is the test for sumArray(); uncomment it to run it
-
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -108,12 +118,14 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+
 function multiplyArray(multArr) { //eslint-disable-line
+    
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
